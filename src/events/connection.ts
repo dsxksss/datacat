6a55227a23f0da8e6c327e689e65428e3a3e7a70
VentCat <2546650292@qq.com>
2023-07-23 12:23:21 +0800

    优化创建连接代码逻辑

--- src/events/connection.ts
+++ src/events/connection.ts
@@ -1,64 +1,48 @@
 import { Webview, window } from 'vscode';
 import logger from '../util/logger';
-import { QueryTypes } from 'sequelize';
+import { QueryTypes, Sequelize } from 'sequelize';
 import { connectionManager, createDBConnection } from '../util/connectionManager';
 import { globalProviderManager } from '../util/globalProviderManager';
 
+const getTableAndColumnData = async (connection: Sequelize, database: any) => {
+    const tables = await connection.query(`SELECT table_name FROM information_schema.tables WHERE table_schema = '${database}'`, { type: QueryTypes.SELECT });
+    const result: { [key: string]: any } = {};
+    const promises = tables.map(async (row: any) => {
+        const tableName = row["TABLE_NAME"];
+        const columns = await connection.query(`SELECT column_name FROM information_schema.columns WHERE table_schema = '${database}' AND table_name = '${tableName}'`, { type: QueryTypes.SELECT });
+        result[tableName] = columns;
+    });
+    await Promise.all(promises);
+
+    return result;
+};
+
 export const createConnectionEvent = async (message: any) => {
+    const { connectionName, database, username, password, host, port, dialect } = message;
     const context = globalProviderManager.get("extensionContext");
     const sidebarWebview: Webview = globalProviderManager.get("sidebarWebview");
 
-    // 当连接存在情况下
-    if (context.globalState.keys().includes(message.connectionName)) {
-        const data = context.globalState.get(message.connectionName);
-        window.showErrorMessage("连接已存在!");
-        sidebarWebview.postMessage(data);
-        return;
+    // 当连接已存在的情况下
+    if (context.globalState.keys().includes(connectionName)) {
+        sidebarWebview.postMessage(context.globalState.get(connectionName));
+        return window.showErrorMessage("连接已存在!");
     }
 
+    const connection = await createDBConnection(database, username, password, host, port, dialect);;
+    connectionManager.addConnection(connectionName, connection);
 
-    const result: { [key: string]: any } = {};
+    const dbData = await getTableAndColumnData(connection, database);
 
-    const { connectionName, database, username, password, host, port, dialect } = message;
-
-    const connection1 = await createDBConnection(database, username, password, host, port, dialect);
-    connectionManager.addConnection(connectionName, connection1);
-
-    const tables = await connection1.query(
-        `SELECT table_name FROM information_schema.tables WHERE table_schema = '${database}'`,
-        {
-            type: QueryTypes.SELECT
-        }
-    );
-
-    const tableNames = tables.map((row: any) => row[`TABLE_NAME`]);
-
-    for (const tableName of tableNames) {
-        const data = await connection1.query(
-            `SELECT column_name FROM information_schema.columns WHERE table_schema = '${database}' AND table_name = '${tableName}'`,
-            {
-                type: QueryTypes.SELECT
-            }
-        );
-
-        result[tableName] = data;
+    const datacatConnectionList = context.globalState.get("datacat-cnnection-list", []);
+    // 当连接不存在连接列表内，则添加该连接
+    if (!datacatConnectionList.includes(connectionName)) {
+        datacatConnectionList.push(connectionName);
     }
 
+    context.globalState.update(connectionName, dbData);
+    context.globalState.update("datacat-cnnection-list", datacatConnectionList);
 
-    window.showInformationMessage(`数据库连接已创建：${connectionName}`);
+    sidebarWebview.postMessage(dbData);
     logger.info(`数据库连接已创建：${connectionName}`);
-
-
-    context.globalState.update(message.connectionName, result);
-    let datacatCnnectionList: string[] = [];
-    if (context.globalState.keys().includes("datacat-cnnection-list")) {
-        datacatCnnectionList = context.globalState.get("datacat-cnnection-list");
-    }
-
-    if (!datacatCnnectionList.includes(connectionName)) {
-        datacatCnnectionList.push(connectionName);
-    }
-
-    context.globalState.update("datacat-cnnection-list", datacatCnnectionList);
-    sidebarWebview.postMessage(result);
+    window.showInformationMessage(`数据库连接已创建：${connectionName}`);
 };
